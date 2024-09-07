b48c375c7f18ace9274f3b9508a5ba5c834806b6
Cabual06 <efcabual09@gmail.com>
2024-09-07 19:36:09 +0800

    Lian

---
+++ src/pages/Users/Female.vue
@@ -0,0 +1,16 @@
+<template>
+    <Indexnav />
+    <v-container>
+        <h1>Female Candidates</h1>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'Female'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/Users/Male.vue
@@ -0,0 +1,14 @@
+<template>
+    <Indexnav />
+    <v-container>
+        <h1>Male Candidates</h1>
+    </v-container>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
